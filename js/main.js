5420be965e17c51852c243c61d46a6fef8d05c5a
V2Riry <45226790+V2Riry@users.noreply.github.com>
2019-09-08 02:26:23 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,13 @@ function getUrlParam(parameter, defaultvalue){
         }
     return urlparameter;
 }
+function close_window() {
+  if (confirm("Close Window?")) {
+    close();
+  }
+}
 var l = getUrlVars()["l"];
 var w = getUrlVars()["w"];
 var h = getUrlVars()["h"];
 window.open(l,'name','width='+w+',height='+h);
-window.close();
+close_window()
