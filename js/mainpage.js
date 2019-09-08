a39538f21d12d6cc0eb675f3d089323769ffb7a8
V2Riry <45226790+V2Riry@users.noreply.github.com>
2019-09-08 16:46:50 -0700

    Update mainpage.js

--- js/mainpage.js
+++ js/mainpage.js
@@ -4,7 +4,7 @@ function myFunction() {
 	var width = document.getElementById("width").value;
 	resultArea = document.getElementById("result");
 	if (/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url) && /^[0-9]*$/.test(height) && /^[0-9]*$/.test(width)) {
-		get_short_url("https://v2riry.github.io/newwinresize.html?l="+url+"&h="+height+"&w="+width, function(short_url) {
+		get_short_url("https://v2riry.github.io/RunHTML/newwinresize.html?l="+url+"&h="+height+"&w="+width, function(short_url) {
 		document.getElementById("result").value =short_url;
 		});
 		if (resultArea.style.display = "none") {
@@ -28,10 +28,10 @@ function get_short_url(long_url, func)
 {
 	$ = jQuery;
     $.getJSON(
-        "http://api.bitly.com/v3/shorten?callback=?", 
+        "https://api-ssl.bitly.com/v3/shorten?", 
         { 
             "format": "json",
-            "apiKey": "R_c6fb57ec4ac344daac703d9a64f65702",
+            "access_token": "R_c6fb57ec4ac344daac703d9a64f65702",
             "login": "v2riry",
             "longUrl": long_url
         },
